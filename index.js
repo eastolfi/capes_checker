fccf5ddf91c6edbc7b8306dbf445777e8285fe0b
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2020-07-22 10:46:42 +0200

    chore: clean up some code

--- index.js
+++ index.js
@@ -17,22 +17,7 @@ const initBot = () => {
     })
     
     bot.on('start', function() {
-        // more information about additional params https://api.slack.com/methods/chat.postMessage
-        
-        
-        // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
-        // bot.postMessageToChannel('general', `¡Hay información disponible! Compruébalo <${url}|*aquí*>`, params);
         console.log('Bot enabled')
-        
-        // define existing username instead of 'user_name'
-        // bot.postMessageToUser('user_name', 'meow!', params); 
-        
-        // If you add a 'slackbot' property, 
-        // you will post to another user's slackbot channel instead of a direct message
-        // bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' }); 
-        
-        // define private group instead of 'private_group', where bot exist
-        // bot.postMessageToGroup('private_group', 'meow!', params); 
     });
     
     return bot
